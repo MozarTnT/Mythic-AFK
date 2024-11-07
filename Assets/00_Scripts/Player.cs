c32bdefd428c37e6eafd27e4543a958b3a6a5507
JYJ <allen321@naver.com>
2024-11-07 23:35:04 +0900

    주석 추가 및 에셋 파일 정리

--- Assets/00_Scripts/Player.cs
+++ Assets/00_Scripts/Player.cs
@@ -17,39 +17,46 @@ public class Player : Character
 
     private void Update()
     {
+        // 타겟이 없을 경우 가장 가까운 타겟을 찾음
         if(m_Target == null)
         {
-            FindClosestTarget(Spawner.m_Monsters.ToArray());
+            FindClosestTarget(Spawner.m_Monsters.Select(m => m.transform).ToArray());
 
+            // 시작 위치로 돌아가는 로직
             float targetPos = Vector3.Distance(transform.position, startPos);
             if(targetPos >= 0.1f)
             {
+                // 이동 및 회전
                 transform.position = Vector3.MoveTowards(transform.position, startPos, Time.deltaTime);
                 transform.LookAt(startPos);
                 AnimatorChange("isMOVE");
             }
             else
             {
+                // 제자리에서 대기
                 transform.rotation = rot;
                 AnimatorChange("isIDLE");
             }
+            
             return;
         }
 
+        // 타겟과의 거리 계산
         float targetDistance = Vector3.Distance(transform.position, m_Target.position);
-        if(targetDistance <= Target_Range && targetDistance > Attack_Range && isAttack == false) // 추적 범위 안에 있지만 공격 범위가 안될때
+        if(targetDistance <= Target_Range && targetDistance > Attack_Range && isAttack == false)
         {
+            // 추적 범위 내에 있을 때 이동
             AnimatorChange("isMOVE");
             transform.LookAt(m_Target.position);
             transform.position = Vector3.MoveTowards(transform.position, m_Target.position, Time.deltaTime);
         }
-        else if(targetDistance <= Attack_Range && isAttack == false) // 공격 범위 안에 있을때
+        else if(targetDistance <= Attack_Range && isAttack == false)
         {
+            // 공격 범위 내에 있을 때 공격
             isAttack = true;
             AnimatorChange("isATTACK");
             Invoke("InitAttack", 1.0f);
         }
-
     }
 
 }
