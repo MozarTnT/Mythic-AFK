b1c39a3042e191649186cb2fdf09d001864ae1a9
JYJ <allen321@naver.com>
2025-02-11 19:04:59 +0900

    Firebase Manager 백업용 파일 수정

--- Assets/00_Scripts/Manager/Firebase/Firebase_Manager.cs
+++ Assets/00_Scripts/Manager/Firebase/Firebase_Manager.cs
@@ -3,20 +3,28 @@ using Firebase.Auth;
 using Firebase.Database;
 using UnityEngine;
 
-public partial class Firebase_Manager
+
+public partial class Firebase_Manager // 파이어베이스 관리자 클래스 (기능 분리를 위해 partial 클래스로 분리)
 {
+    // 파이어베이스 인증 객체
     private FirebaseAuth auth;
+    // 현재 로그인한 사용자 객체
     private FirebaseUser currentUser;
+    // 파이어베이스 데이터베이스 참조 객체
     public DatabaseReference reference;
+
     public void Init()
     {
-        // 파이어베이스 초기화
+        // Firebase SDK의 모든 필수 구성요소가 있는지 확인하고 없으면 수정
         FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => 
         {
             if(task.Result == DependencyStatus.Available)
             {
+                // 파이어베이스 인증 객체 초기화
                 auth = FirebaseAuth.DefaultInstance;
+                // 현재 로그인한 사용자 객체 초기화
                 currentUser = auth.CurrentUser;
+                // 파이어베이스 데이터베이스 참조 객체 초기화
                 reference = FirebaseDatabase.DefaultInstance.RootReference;
 
                 GuestLogin();
@@ -28,6 +36,4 @@ public partial class Firebase_Manager
             }
         });
     }
-
-    
 }
